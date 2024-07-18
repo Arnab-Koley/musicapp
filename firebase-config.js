a3d72809e3c8652ef35a3e33d86d97f8bc8dc98c
Arnab Koley <arnab123koley@gmail.com>
2024-07-19 00:21:13 +0530

    first commit

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBiAogTI93hZB5VsAOdUZiLuULy5tCSOrI",
+  authDomain: "musicapp-2a36e.firebaseapp.com",
+  projectId: "musicapp-2a36e",
+  storageBucket: "musicapp-2a36e.appspot.com",
+  messagingSenderId: "3568400353",
+  appId: "1:3568400353:web:f66839d28120878eeac4a0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='w-full h-12 flex justify-center items-center text-xl font-bold text-white bg-slate-700 max-md:bg-yellow-500 max-md:text-red-500'>
+        Music App
+        bj bjkzxc bjkzscjksc
+        ascvhacvhjchv
+        casjcascascbvbhuj
+      </div>
+    </>
+  )
+}
+
+export default App
